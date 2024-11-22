25127c31c3270931f272c3363d8a9af1e8b7e66d
Oleg Kudryashov <olegkudryashov.irk@gmail.com>
2024-11-22 19:43:43 +0800

    final schemas.org owl file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from rdflib import Graph
 from ontology_utils import extract_and_save_classes
 
 g = Graph()
-g.parse("schemaorg.owl", format="xml")
+g.parse("schemas\schemaorg.owl", format="xml")
 
 file_path = extract_and_save_classes(g)
 print(f"Список классов сохранён в файл: {file_path}")
