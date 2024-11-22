c03c3e269d1a01bd9f43dd692877e5d08fa85c72
Oleg Kudryashov <olegkudryashov.irk@gmail.com>
2024-11-22 19:32:10 +0800

    schemas.org big .owl file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from rdflib import Graph
 from ontology_utils import extract_and_save_classes
 
-# Загружаем онтологию
 g = Graph()
 g.parse("schemaorg.owl", format="xml")
 
-# Извлекаем и сохраняем классы
 file_path = extract_and_save_classes(g)
 print(f"Список классов сохранён в файл: {file_path}")
--- ontology_utils.py
+++ ontology_utils.py
@@ -2,21 +2,17 @@ import os
 from rdflib.namespace import RDFS
 
 def extract_and_save_classes(graph, file_name="classes.txt"):
-    # Список для хранения классов
+    
     classes = []
 
-    # Извлекаем классы из онтологии
     for subject, predicate, obj in graph.triples((None, RDFS.subClassOf, None)):
-        classes.append(str(subject))  # Преобразуем URI в строку
+        classes.append(str(subject))  
 
-    # Сортируем список классов
     sorted_classes = sorted(classes)
 
-    # Определяем путь для сохранения файла
     work_dir = os.getcwd()
     file_path = os.path.join(work_dir, file_name)
 
-    # Записываем отсортированный список в файл
     with open(file_path, "w", encoding="utf-8") as file:
         file.write("\n".join(sorted_classes))
 
